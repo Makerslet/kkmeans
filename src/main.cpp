c2fc60c8caab29b51ce90c96614973b41199012a
makerslet <oshuev.valery@yandex.ru>
2020-05-23 16:18:17 +0300

    Пустой коммит

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@ using namespace dlib;
 
 int main()
 {
+
     // Here we declare that our samples will be 2 dimensional column vectors.
     // (Note that if you don't know the dimensionality of your vectors at compile time
     // you can change the 2 to a 0 and then set the size at runtime)
